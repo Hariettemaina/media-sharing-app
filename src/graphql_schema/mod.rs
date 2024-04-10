330240039b076010893bc84a4f36e1df9ea88c92
Hariette Maina <hariettemaina@gmail.com>
2024-04-10 17:04:11 +0300

    correct spelling mistake

--- src/graphql_schema/mod.rs
+++ src/graphql_schema/mod.rs
@@ -2,7 +2,7 @@ use async_graphql::SimpleObject;
 
 pub mod users;
 
-use crate::graphql_schema::users::mutaion::UserMut;
+use crate::graphql_schema::users::mutation::UserMut;
 
 #[derive(SimpleObject,Default)]
 pub struct Mutation {
